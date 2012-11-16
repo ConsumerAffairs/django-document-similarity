1aecbe91db353f76aa20877bda84cc449c12156a
John Whitlock <John-Whitlock@ieee.org>
2012-11-16 12:38:46 -0600

    Tests for buffer_html_document

--- docsim/api/tests.py
+++ docsim/api/tests.py
@@ -1,16 +1,48 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+import mox
+
+from api import views
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class MockSimServerTestCase(TestCase):
+    '''Base class for API tests'''
+
+    def setUp(self):
+        super(MockSimServerTestCase, self).setUp()
+        self.mox = mox.Mox()
+        self.mox.StubOutWithMock(views, 'get_service')  # For safety
+
+    def tearDown(self):
+        super(MockSimServerTestCase, self).tearDown()
+        self.mox.VerifyAll()
+        self.mox.UnsetStubs()
+
+    def mox_service(self):
+        service = self.mox.CreateMockAnything()
+        views.get_service().AndReturn(service)
+        return service
+
+
+class BufferHTMLDocTest(MockSimServerTestCase):
+
+    def test_buffer_doc(self):
+        post = {
+            'id': 1,
+            'html': '<p>a simple document</p>'
+        }
+
+        service = self.mox_service()
+        service.buffer([{
+            'id': '1',
+            'tokens': ['simple', 'document']}])
+
+        self.mox.ReplayAll()
+        response = self.client.post(reverse('buffer_html'), post)
+        self.mox.VerifyAll()
+
+        self.assertEqual(response.status_code, 202)
+
+    def test_bad_buffer_call(self):
+        response = self.client.post(reverse('buffer_html'), {})
+        self.assertEqual(response.status_code, 400)
